b466cc18842cd80c5cde5aa710254962aeedd016
Ergash Aliy <ergashalimuhammadjanov@gmail.com>
2026-02-25 12:16:28 +0500

    my-first

---
+++ src/App.jsx
@@ -0,0 +1,356 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='body'>
+      <div className="hero">
+        <div className="container">
+          <div className="heroInfo">
+            <h1>
+              This is your designer
+              <span>Esther Howard</span>
+              <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+              <button>Discuss for Projects</button>
+            </h1>
+          </div>
+          <img src="./public/imgs/heroimgs.png" alt="" />
+        </div>
+
+      </div>
+
+      <main>
+        <section className='space'>
+          <h3>SPECIALITY</h3>
+          <h1><span class="rang">My</span> <span class="rang2">specialities</span></h1>
+          <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+          <div className="cards">
+            <div className="box">
+              <div className="rasim">
+                <img className='rasim1' src="./public/imgs/mark_email_unread.svg" alt=""></img>
+                <img className='rasim2' src="./public/imgs/Subtract.svg" alt=""></img>
+              </div>
+              <h2>Email marketing</h2>
+              <img className='chiziq' src="./public/imgs/chiziq.svg" alt="" />
+              <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+            </div>
+            <div className="box">
+              <div className="rasim">
+                <img className='rasim1' src="./public/imgs/quti.svg" alt=""></img>
+                <img className='rasim2' src="./public/imgs/Subtract.svg" alt=""></img>
+              </div>
+              <h2>Market analysis</h2>
+              <img className='chiziq' src="./public/imgs/chiziq.svg" alt="" />
+              <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+            </div>
+            <div className="box">
+              <div className="rasim">
+                <img className='rasim1' src="./public/imgs/query_stats.svg" alt=""></img>
+                <img className='rasim2' src="./public/imgs/Subtract.svg" alt=""></img>
+              </div>
+              <h2>Keyword research</h2>
+              <img className='chiziq' src="./public/imgs/chiziq.svg" alt="" />
+              <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+            </div>
+          </div>
+        </section>
+        <section className='process'>
+          <h3>ABOUT ME</h3>
+          <h1><span class="rang">My</span> <span class="rang2">work process</span></h1>
+          <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+          <div className="triangle">
+            <div className="uchburchaklar1">
+              <div className="uchburchak1">
+                <img className='aylana' src="./public/imgs/Rectangle 57.svg" alt="" />
+                <img className='uchburchak' src="./public/imgs/Rectangle 56.svg" alt="" />
+                <h2>Empathize</h2>
+                <img className='past1' src="./public/imgs/past.svg" alt="" />
+              </div>
+              <div className="uchburchak1">
+                <img className='aylana' src="./public/imgs/Rectangle 57.svg" alt="" />
+                <img className='uchburchak' src="./public/imgs/Rectangle 56.svg" alt="" />
+                <h2>Ideate</h2>
+                <img className='tepa1' src="./public/imgs/tepa.svg" alt="" />
+
+              </div>
+              <div className="uchburchak1">
+                <img className='aylana' src="./public/imgs/Rectangle 57.svg" alt="" />
+                <img className='uchburchak' src="./public/imgs/Rectangle 56.svg" alt="" />
+                <h2>Qualitative</h2>
+                <img className='past2' src="./public/imgs/past.svg" alt="" />
+              </div>
+            </div>
+
+
+            <div className="uchburchaklar2">
+              <div className="uchburchak1">
+                <img className='aylana' src="./public/imgs/Rectangle 57.svg" alt="" />
+                <img className='uchburchak' src="./public/imgs/Rectangle 56.svg" alt="" />
+                <h2>Define</h2>
+                <img className='tepa2' src="./public/imgs/tepa.svg" alt="" />
+
+              </div>
+              <div className="uchburchak1">
+                <img className='aylana' src="./public/imgs/Rectangle 57.svg" alt="" />
+                <img className='uchburchak' src="./public/imgs/Rectangle 56.svg" alt="" />
+                <h2>Prototype</h2>
+                <img className='past3' src="./public/imgs/past.svg" alt="" />
+              </div>
+              <div className="uchburchak1">
+                <img className='aylana' src="./public/imgs/Rectangle 57.svg" alt="" />
+                <img className='uchburchak' src="./public/imgs/Rectangle 56.svg" alt="" />
+                <h2>Quantitative</h2>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className='service'>
+          <h3>SERVICES</h3>
+          <h1><span class="rang">My</span> <span class="rang2">other services</span></h1>
+          <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+          <div className="cards">
+            <div className="card">
+              <img className='teparasim' src="./public/imgs/Group 47450.svg" alt="" />
+              <h2>Digital branding</h2>
+              <img src="./public/imgs/chiziq.svg" alt="" />
+              <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.</p>
+              <button>Discuss for Projects <img src="./public/imgs/Arrow 7.svg" alt="" /></button>
+            </div>
+            <div className="card">
+              <img className='teparasim' src="./public/imgs/r1.svg" alt="" />
+              <h2>Creative solutions</h2>
+              <img src="./public/imgs/chiziq.svg" alt="" />
+              <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.</p>
+              <button>Discuss for Projects <img src="./public/imgs/Arrow 7.svg" alt="" /></button>
+            </div>
+            <div className="card">
+              <img className='teparasim' src="./public/imgs/r2.svg" alt="" />
+              <h2>Marketing solutions</h2>
+              <img src="./public/imgs/chiziq.svg" alt="" />
+              <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.</p>
+              <button>Discuss for Projects <img src="./public/imgs/Arrow 7.svg" alt="" /></button>
+            </div>
+            <div className="card">
+              <img className='teparasim' src="./public/imgs/r3.svg" alt="" />
+              <h2>Business analytics</h2>
+              <img src="./public/imgs/chiziq.svg" alt="" />
+              <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.</p>
+              <button>Discuss for Projects <img src="./public/imgs/Arrow 7.svg" alt="" /></button>
+            </div>
+            <div className="card">
+              <img className='teparasim' src="./public/imgs/r4.svg" alt="" />
+              <h2>UI & UX Design</h2>
+              <img src="./public/imgs/chiziq.svg" alt="" />
+              <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.</p>
+              <button>Discuss for Projects <img src="./public/imgs/Arrow 7.svg" alt="" /></button>
+            </div>
+            <div className="card">
+              <img className='teparasim' src="./public/imgs/r5.svg" alt="" />
+              <h2>Photography</h2>
+              <img src="./public/imgs/chiziq.svg" alt="" />
+              <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.</p>
+              <button>Discuss for Projects <img src="./public/imgs/Arrow 7.svg" alt="" /></button>
+            </div>
+          </div>
+        </section>
+        <section className="portfolios">
+          <div className="container">
+            <h4>PORTFOLIOS</h4>
+            <h3>
+              My <span>completed projects</span>
+            </h3>
+            <p>
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+              sint. Velit officia consequat duis enim velit mollit.
+            </p>
+            <div className="bgImg">
+              <img src="imgs/PORTFOLIOS.png" alt="" />
+              <button>
+                <p>View all Projects</p>
+              </button>
+            </div>
+          </div>
+        </section>
+      </main>
+      <section className="serviceee">
+        <div className="container">
+          <div className="boxInfo">
+            <div className="img">
+              <img src="imgs/img.png" alt="" />
+              <img src="imgs/Group.svg" alt="" />
+            </div>
+            <div className="info">
+              <h3>“Service is good and I recommend”</h3>
+              <p>
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.Amet minim
+                mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+              </p>
+              <p>
+                Velit officia consequat duis enim velit mollit. Exercitation
+                veniam consequat sunt nostrud amet.Amet minim mollit non
+                deserunt ullamco est sit aliqua dolor do amet sint. Velit
+                officia consequat duis enim velit mollit.
+              </p>
+              <p>
+                Exercitation veniam consequat sunt nostrud amet.Amet minim
+                mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+              </p>
+              <img src="imgs/imzo.svg" alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="teams">
+        <div className="container">
+          <h4>TEAMS</h4>
+          <h3>
+            My <span>team members</span>
+          </h3>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit.
+          </p>
+          <div className="cards">
+            <div className="box">
+              <img src="imgs/ui1.png" alt="" />
+              <h1>UI & UX Design</h1>
+              <p>
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint.
+              </p>
+            </div>
+            <div className="box">
+              <img src="imgs/ui2.png" alt="" />
+              <h1>UI & UX Design</h1>
+              <p>
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint.
+              </p>
+            </div>
+            <div className="box">
+              <img src="imgs/ui3.png" alt="" />
+              <h1>UI & UX Design</h1>
+              <p>
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint.
+              </p>
+            </div>
+            <div className="box">
+              <img src="imgs/ui4.png" alt="" />
+              <h1>UI & UX Design</h1>
+              <p>
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="telegram">
+        <div className="tel_cards">
+          <img className="bg" src="imgs/bg.png" alt="" />
+          <div className="tel_box">
+            <img src="imgs/tg.png" alt="" />
+            <div>
+              <h1>Have any project idea?</h1>
+              <p>
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint.
+              </p>
+            </div>
+          </div>
+          <div className="tel_btn">
+            <button>Contact Now</button>
+          </div>
+        </div>
+      </section>
+      <footer>
+        <div className="container">
+          <div className="footer_cards">
+            <div className="footer_box">
+              <img src="imgs/logo.svg" alt="" />
+              <p>
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.
+              </p>
+              <div className="icons">
+                <img src="imgs/in.svg" alt="" />
+                <img src="imgs/ft.svg" alt="" />
+                <img src="imgs/tt.svg" alt="" />
+                <img src="imgs/yt.svg" alt="" />
+              </div>
+            </div>
+            <div className="footer_box">
+              <h2>Quick link</h2>
+              <ul>
+                <li>
+                  <a href=""></a>Home
+                </li>
+                <li>
+                  <a href=""></a>Service
+                </li>
+                <li>
+                  <a href=""></a>Projects
+                </li>
+                <li>
+                  <a href=""></a>About
+                </li>
+                <li>
+                  <a href=""></a>Client
+                </li>
+                <li>
+                  <a href=""></a>Contact
+                </li>
+              </ul>
+            </div>
+            <div className="footer_box">
+              <h2>Important</h2>
+              <ul>
+                <li>
+                  <a href=""></a>Career
+                </li>
+                <li>
+                  <a href=""></a>Terms & Conditions
+                </li>
+                <li>
+                  <a href=""></a>Privacy Policy
+                </li>
+                <li>
+                  <a href=""></a>Cookies Policy
+                </li>
+                <li>
+                  <a href=""></a>Social Work
+                </li>
+              </ul>
+            </div>
+            <div className="footer_box">
+              <h2>Contacts</h2>
+              <div>
+                <div className="tel">
+                  <img src="imgs/call.svg" alt="" />
+                  <span>+223 456 789</span>
+                </div>
+                <div className="tel">
+                  <img src="imgs/send.svg" alt="" />
+                  <span>example@gmail.com</span>
+                </div>
+                <div className="tel">
+                  <img src="imgs/location.svg" alt="" />
+                  <span>6391 Elgin St. Celina, Delaware</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <p>Copyright 2022 | All Rights Reserved</p>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default App
